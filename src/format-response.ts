31a40c7ecda002e5b4f6465e8bb73949a4c921ce
Anikait <anikait.makkar@outlook.com>
2021-05-28 15:28:17 +0530

    first commit

---
+++ src/format-response.ts
@@ -0,0 +1,41 @@
+import { Appointment, SessionResponse } from "../@types";
+
+
+/**
+ * Formats the date from 23/5/2021 -> 23-5-2021
+ * @param date
+ * @returns
+ */
+export const formatDate = (date: Date) =>
+  date.toLocaleDateString("en-IN").replace(/\//g, "-");
+
+/**
+ * Maps the plain session response to appointment object
+ * @param { SessionResponse } session
+ * @returns { Appointment }
+ */
+export const mapSessionResponse = ({
+  session_id,
+  date,
+  available_capacity,
+  available_capacity_dose1,
+  available_capacity_dose2,
+  slots,
+  min_age_limit,
+  vaccine,
+  ...center
+}: SessionResponse): Appointment => ({
+  ...center,
+  sessions: [
+    {
+      session_id,
+      date,
+      slots,
+      min_age_limit,
+      vaccine,
+      available_capacity,
+      available_capacity_dose1,
+      available_capacity_dose2,
+    },
+  ],
+});
---
+++ src/index.ts
@@ -0,0 +1,171 @@
+import superagent from "superagent";
+import {
+  AppointmentResponse,
+  Appointment,
+  BadRequest,
+  Options,
+  State,
+  District,
+} from "../@types";
+import { formatDate, mapSessionResponse } from "./format-response";
+
+const URLS = {
+  BASE_URL: "https://cdn-api.co-vin.in/api/v2",
+  FIND_BY_PIN: "appointment/sessions/public/findByPin",
+  FIND_BY_DISTRICT: "appointment/sessions/public/findByDistrict",
+  CALENDAR_BY_PIN: "appointment/sessions/public/calendarByPin",
+  CALENDAR_BY_DISTRICT: "appointment/sessions/public/calendarByDistrict",
+  GET_STATES: "admin/location/states",
+  GET_DISTRICTS: "admin/location/districts",
+};
+
+/**
+ * Find vaccination sessions by PIN.
+ * By default searches for current date.
+ * @param { string | number } pin
+ * @param { Date } date
+ */
+const findAppointmentsByPinDay = async (
+  pin: string | number,
+  date: Date = new Date()
+): Promise<AppointmentResponse> => {
+  const url = `${URLS.BASE_URL}/${URLS.FIND_BY_PIN}`;
+  try {
+    const response = await superagent
+      .get(url)
+      .query({ pincode: pin, date: formatDate(date) });
+
+    const appointments: Appointment[] =
+      response.body.sessions.map(mapSessionResponse);
+    return { appointments, error: null, isError: false };
+  } catch (err) {
+    const badRequest: BadRequest = err.response?.body;
+    return { appointments: null, error: badRequest, isError: true };
+  }
+};
+
+/**
+ * Find vaccination sessions by district.
+ * By default searches for current date
+ * @param { number } districtId
+ * @param { Date } date
+ */
+const findAppointmentsByDistrictDay = async (
+  districtId: number,
+  date: Date = new Date()
+): Promise<AppointmentResponse> => {
+  const url = `${URLS.BASE_URL}/${URLS.FIND_BY_DISTRICT}`;
+
+  try {
+    const response = await superagent
+      .get(url)
+      .query({ district_id: districtId, date: formatDate(date) });
+
+    const appointments: Appointment[] =
+      response.body.sessions.map(mapSessionResponse);
+    return { appointments, error: null, isError: false };
+  } catch (err) {
+    const badRequest: BadRequest = err.response?.body;
+    return { appointments: null, error: badRequest, isError: true };
+  }
+};
+
+/**
+ * Find vaccine sessions by pin for a week.
+ * By default week starts from current date
+ * @param { string | number } pin
+ * @param { Date } date
+ */
+const findAppointmentsByPinWeek = async (
+  pin: string | number,
+  date: Date = new Date()
+): Promise<AppointmentResponse> => {
+  const url = `${URLS.BASE_URL}/${URLS.CALENDAR_BY_PIN}`;
+
+  try {
+    const response = await superagent
+      .get(url)
+      .query({ pincode: pin, date: formatDate(date) });
+
+    const appointments: Appointment[] = response.body.centers;
+    return { appointments, error: null, isError: false };
+  } catch (err) {
+    return { appointments: null, error: null, isError: false };
+  }
+};
+
+/**
+ * Find vaccine sessions by district id for a week.
+ * By default week starts from current date
+ * @param { number } districtId
+ * @param { Date } date
+ */
+const findAppointmentsByDistrictWeek = async (
+  districtId: number,
+  date: Date = new Date()
+): Promise<AppointmentResponse> => {
+  const url = `${URLS.BASE_URL}/${URLS.FIND_BY_DISTRICT}`;
+
+  try {
+    const response = await superagent
+      .get(url)
+      .query({ district_id: districtId, date: formatDate(date) });
+
+    const appointments: Appointment[] = response.body.centers;
+    return { appointments, error: null, isError: false };
+  } catch (err) {
+    const badRequest: BadRequest = err.response?.body;
+    return { appointments: null, error: badRequest, isError: true };
+  }
+};
+
+/**
+ * Search for appointments using pin.
+ * @param { string | number } pin
+ * @param { Options } opts Allows you to switch between per day or per week results
+ */
+export const findAppointmentsByPin = (
+  pin: string | number,
+  opts: Options
+): Promise<AppointmentResponse> => {
+  const { date = new Date(), week = false } = opts;
+  if (week) {
+    return findAppointmentsByPinWeek(pin, date);
+  } else {
+    return findAppointmentsByPinDay(pin, date);
+  }
+};
+
+/**
+ * Search for appointments using district.
+ * @param { string | number } pin
+ * @param { Options } opts Allows you to switch between per day or per week results
+ */
+export const findAppointmentsByDistrict = (
+  districtId: number,
+  opts: Options
+): Promise<AppointmentResponse> => {
+  const { date = new Date(), week = false } = opts;
+  if (week) {
+    return findAppointmentsByDistrictWeek(districtId, date);
+  } else {
+    return findAppointmentsByDistrictDay(districtId, date);
+  }
+};
+
+/**
+ * Fetch the list of available states.
+ */
+export const getStates = (): Promise<State[]> => {
+  const url = `${URLS.BASE_URL}/${URLS.GET_STATES}`;
+  return superagent.get(url).then((response) => response.body.states);
+};
+
+/**
+ * Fetch districts corresponding to the given state id.
+ * @param { number } stateId
+ */
+export const getDistricts = (stateId: number): Promise<District> => {
+  const url = `${URLS.BASE_URL}/${URLS.GET_DISTRICTS}/${stateId}`;
+  return superagent.get(url).then((response) => response.body.districts);
+};
